f452f451790ce74a4ed4a7dd3540b8527bccf4e7
Sciss <contact@sciss.de>
2012-08-17 18:34:14 +0100

    ...

--- src/test/scala/de/sciss/macrocosm/MacrocosmTest.scala
+++ src/test/scala/de/sciss/macrocosm/MacrocosmTest.scala
@@ -3,6 +3,18 @@ package de.sciss.macrocosm
 object MacrocosmTest extends App {
    import Macrocosm._
 
-   println( "Aqui" )
+//   println( "Aqui" )
+
+   object WhiteNoiseAr extends GE { val rate = audio }
+
+   println( "---- anon ----" )
    LPF.ar( new GE { val rate = audio })
+   println( "---- WhiteNoise.ar ----" )
+   LPF.ar( WhiteNoiseAr )
+   println( "---- val -> WhiteNoise.ar ----" )
+   val wn = WhiteNoiseAr
+   LPF.ar( wn )
+   println( "---- val:GE -> WhiteNoise.ar ----" )
+   val wn2: GE = WhiteNoiseAr
+   LPF.ar( wn2 )
 }
